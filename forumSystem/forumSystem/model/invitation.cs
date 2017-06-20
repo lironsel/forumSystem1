2532d1ce3c30ac4d432d20096cc78976fd5d8b4d
nitzan <nitzanmassad@gmail.com>
2017-06-20 20:19:34 +0300

    invitation

---
+++ forumSystem/forumSystem/model/invitation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace forumSystem.model
+{
+    class invitation
+    {
+        User received_by;
+        User sent_by;
+        Group invites_to;
+
+
+        public void inviate_addFrind(String userInviteID, String userInvitedID) { }
+        public void creatInvitation() { }
+    }
+}
