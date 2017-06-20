1a3b27c10c61db3a66b06cfe3d796db86577b3b1
nitzan <nitzanmassad@gmail.com>
2017-06-20 20:12:51 +0300

    Content

---
+++ forumSystem/forumSystem/model/Content.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace forumSystem.model
+{
+    class Content
+    {
+        ThreadMessage assembling2;
+        private String contents;
+        public void putContent(String content) { }
+    }
+}
