16f6f34eb1fab2020abd6b215d4a58191e7b0ace
nitzan <nitzanmassad@gmail.com>
2017-06-20 21:44:11 +0300

    Title

---
+++ forumSystem/forumSystem/model/Title.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace forumSystem.model
+{
+    class Title
+    {
+
+        ThreadMessage assembling;
+        private String title;
+        public void putTitle() { }
+
+    }
+}
