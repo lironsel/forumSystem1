8b5019cf59eac1b5a5857a701255dd3e81ddef52
lironsel <lironseliktar@gmail.com>
2017-06-20 19:54:03 +0300

    add files

---
+++ forumSystem/forumSystem/ForumSystem.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Forms;
+
+namespace forumSystem
+{
+    public partial class ForumSystem : Form
+    {
+        private static ForumSystem instance;
+
+        private ForumSystem()
+        {
+            InitializeComponent();
+        }
+
+        public static ForumSystem Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new ForumSystem();
+                }
+                return instance;
+            }
+        }
+    }
+}
---
+++ forumSystem/forumSystem/model/User.cs
@@ -0,0 +1,33 @@
+﻿using forumSystem.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace forumSystem
+{
+    class User : Guest
+    {
+        //Forum signedUp;
+        List<Complaint> complain;
+        List<User> friend_of;
+        List<Group> included;
+        List<ThreadMessage> publish;
+        List<User> Friendly_with;
+        List<Message> send;
+        List<Notification> receive;
+        invitation received;
+        List<friendRequest> frined_request_to_others;
+        List<friendRequest> recive;
+        string m_name;
+        string m_password;
+        bool m_active;
+        public User(string name, string password)
+        {
+            m_active = true;
+            m_name = name;
+            m_password = password;
+        }
+    }
+}
