c08273244f34e604e0ad2ba2cd0430c56124fdaa
nitzan <nitzanmassad@gmail.com>
2017-06-20 21:28:15 +0300

    Notification

---
+++ forumSystem/forumSystem/model/Notification.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace forumSystem.model
+{
+    class Notification
+    {
+
+        List<User> sent_to;
+        private DateTime date;
+
+
+        public void createNotificuition() { }
+    }
+}
