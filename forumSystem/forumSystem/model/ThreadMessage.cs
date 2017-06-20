6f89949f180c731ebf1c40b6913e5ca5bb771a80
adar ovadya <adarovadya@gmail.com>
2017-06-20 21:33:15 +0300

    ThreadMessage

---
+++ forumSystem/forumSystem/model/ThreadMessage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace forumSystem.model
+{
+    class ThreadMessage
+    {
+
+        Thread assembling;
+        Title contains;
+        User posted_by;
+        Content contains2;
+        ThreadMessage replied_on;
+        Collection<ThreadMessage> commented_on_it;
+        private Date CreateDate;
+
+        public void createThreadMessage() { }
+        public String getMessageRepliedOn() { return ""; }
+    }
+}
