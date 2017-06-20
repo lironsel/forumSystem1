1b5b4918a97b6abc4aeac8c1619131ae9512ca70
adar ovadya <adarovadya@gmail.com>
2017-06-20 20:07:35 +0300

    comlaint

---
+++ forumSystem/forumSystem/model/Complaint.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace forumSystem.model
+{
+    class Complaint
+    {
+        Moderator refers_to;
+        User submitted_by;
+        private String complaint;
+
+        public void createComplaint(String complaintID, String user1, String user2) { }
+    }
+}
