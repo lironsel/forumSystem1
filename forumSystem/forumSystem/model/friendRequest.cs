08a8b2d978f523eaad271e5e2a5b7ff44fd64435
adar ovadya <adarovadya@gmail.com>
2017-06-20 20:17:57 +0300

    complaint + friendRequest

---
+++ forumSystem/forumSystem/model/friendRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace forumSystem.model
+{
+    class friendRequest
+    {
+        User friend_request_to_me;
+        User recived_by;
+    }
+}
