9fa89835bab016c392388920570da6ebcb2b1ccc
adar ovadya <adarovadya@gmail.com>
2017-06-22 22:24:04 +0300

    signUp files

---
+++ forumSystem/forumSystem/View/SignUpPanel.Designer.cs
@@ -0,0 +1,272 @@
+﻿using System;
+
+namespace forumSystem.View
+{
+    partial class SignUpPanel
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.signIn_panel = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.email_textBox = new System.Windows.Forms.TextBox();
+            this.birthday_textBox = new System.Windows.Forms.TextBox();
+            this.sex_textBox = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.password_textBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.name_textBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.sign_up_btn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.signIn_panel.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // signIn_panel
+            // 
+            this.signIn_panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(53)))), ((int)(((byte)(65)))));
+            this.signIn_panel.Controls.Add(this.tableLayoutPanel1);
+            this.signIn_panel.Location = new System.Drawing.Point(3, 3);
+            this.signIn_panel.Name = "signIn_panel";
+            this.signIn_panel.Size = new System.Drawing.Size(975, 505);
+            this.signIn_panel.TabIndex = 34;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 390F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 222F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 364F));
+            this.tableLayoutPanel1.Controls.Add(this.email_textBox, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.birthday_textBox, 1, 5);
+            this.tableLayoutPanel1.Controls.Add(this.sex_textBox, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.label8, 0, 5);
+            this.tableLayoutPanel1.Controls.Add(this.password_textBox, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label5, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label7, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.name_textBox, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label6, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.sign_up_btn, 1, 8);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 2, 8);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 9;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 19F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 41F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(975, 505);
+            this.tableLayoutPanel1.TabIndex = 41;
+            // 
+            // email_textBox
+            // 
+            this.email_textBox.BackColor = System.Drawing.Color.Gainsboro;
+            this.email_textBox.Location = new System.Drawing.Point(393, 43);
+            this.email_textBox.Name = "email_textBox";
+            this.email_textBox.Size = new System.Drawing.Size(216, 20);
+            this.email_textBox.TabIndex = 29;
+            this.email_textBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // birthday_textBox
+            // 
+            this.birthday_textBox.BackColor = System.Drawing.Color.Gainsboro;
+            this.birthday_textBox.Location = new System.Drawing.Point(393, 203);
+            this.birthday_textBox.Name = "birthday_textBox";
+            this.birthday_textBox.Size = new System.Drawing.Size(216, 20);
+            this.birthday_textBox.TabIndex = 39;
+            this.birthday_textBox.Text = "dd/mm/yyyy";
+            this.birthday_textBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.birthday_textBox.Click += new System.EventHandler(this.birthday_textBox_Click);
+            // 
+            // sex_textBox
+            // 
+            this.sex_textBox.BackColor = System.Drawing.Color.Gainsboro;
+            this.sex_textBox.Location = new System.Drawing.Point(393, 163);
+            this.sex_textBox.Name = "sex_textBox";
+            this.sex_textBox.Size = new System.Drawing.Size(216, 20);
+            this.sex_textBox.TabIndex = 40;
+            this.sex_textBox.Text = "f/m";
+            this.sex_textBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.sex_textBox.Click += new System.EventHandler(this.sex_textBox_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Dock = System.Windows.Forms.DockStyle.Right;
+            this.label8.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(229)))), ((int)(((byte)(126)))), ((int)(((byte)(49)))));
+            this.label8.Location = new System.Drawing.Point(291, 200);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(96, 40);
+            this.label8.TabIndex = 36;
+            this.label8.Text = "Birthday :";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // password_textBox
+            // 
+            this.password_textBox.BackColor = System.Drawing.Color.Gainsboro;
+            this.password_textBox.Location = new System.Drawing.Point(393, 83);
+            this.password_textBox.Name = "password_textBox";
+            this.password_textBox.Size = new System.Drawing.Size(216, 20);
+            this.password_textBox.TabIndex = 30;
+            this.password_textBox.Text = "8 characters";
+            this.password_textBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.password_textBox.Click += new System.EventHandler(this.password_textBox_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Dock = System.Windows.Forms.DockStyle.Right;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(229)))), ((int)(((byte)(126)))), ((int)(((byte)(49)))));
+            this.label5.Location = new System.Drawing.Point(321, 40);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 40);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Email :";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Dock = System.Windows.Forms.DockStyle.Right;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(229)))), ((int)(((byte)(126)))), ((int)(((byte)(49)))));
+            this.label4.Location = new System.Drawing.Point(282, 80);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(105, 40);
+            this.label4.TabIndex = 28;
+            this.label4.Text = "Password :";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Dock = System.Windows.Forms.DockStyle.Right;
+            this.label7.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(229)))), ((int)(((byte)(126)))), ((int)(((byte)(49)))));
+            this.label7.Location = new System.Drawing.Point(332, 160);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(55, 40);
+            this.label7.TabIndex = 35;
+            this.label7.Text = "Sex  :";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // name_textBox
+            // 
+            this.name_textBox.BackColor = System.Drawing.Color.Gainsboro;
+            this.name_textBox.Location = new System.Drawing.Point(393, 123);
+            this.name_textBox.Name = "name_textBox";
+            this.name_textBox.Size = new System.Drawing.Size(216, 20);
+            this.name_textBox.TabIndex = 34;
+            this.name_textBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Dock = System.Windows.Forms.DockStyle.Right;
+            this.label6.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(229)))), ((int)(((byte)(126)))), ((int)(((byte)(49)))));
+            this.label6.Location = new System.Drawing.Point(279, 120);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(108, 40);
+            this.label6.TabIndex = 33;
+            this.label6.Text = "Full Name :";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // sign_up_btn
+            // 
+            this.sign_up_btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(53)))), ((int)(((byte)(65)))));
+            this.sign_up_btn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.sign_up_btn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.sign_up_btn.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold);
+            this.sign_up_btn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(229)))), ((int)(((byte)(126)))), ((int)(((byte)(49)))));
+            this.sign_up_btn.Location = new System.Drawing.Point(393, 302);
+            this.sign_up_btn.Name = "sign_up_btn";
+            this.sign_up_btn.Size = new System.Drawing.Size(216, 43);
+            this.sign_up_btn.TabIndex = 27;
+            this.sign_up_btn.Text = "Sign Up";
+            this.sign_up_btn.UseVisualStyleBackColor = false;
+            this.sign_up_btn.Click += new System.EventHandler(this.sign_up_btn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(229)))), ((int)(((byte)(126)))), ((int)(((byte)(49)))));
+            this.label1.Location = new System.Drawing.Point(615, 299);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(345, 44);
+            this.label1.TabIndex = 41;
+            this.label1.Text = "Password  must contain 8 characters and at leaste one letter.";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // SignUpPanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.signIn_panel);
+            this.Name = "SignUpPanel";
+            this.Size = new System.Drawing.Size(983, 510);
+            this.signIn_panel.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        
+
+
+
+        #endregion
+
+        private System.Windows.Forms.Panel signIn_panel;
+        private System.Windows.Forms.TextBox sex_textBox;
+        private System.Windows.Forms.TextBox birthday_textBox;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox name_textBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox password_textBox;
+        private System.Windows.Forms.TextBox email_textBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button sign_up_btn;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+    }
+}
