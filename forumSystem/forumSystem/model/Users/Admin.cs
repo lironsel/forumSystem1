0c946d884dd5d0c7d0d467e26f8e9ff6f9eab388
Liron Avraham <liron_avraham@hotmail.com>
2017-06-25 03:24:57 +0300

    some fixes

--- forumSystem/forumSystem/model/Users/Admin.cs
+++ forumSystem/forumSystem/model/Users/Admin.cs
@@ -7,9 +7,6 @@ namespace forumSystem.model
     [Serializable][JsonObject]
     public class Admin : User
     {
-        [JsonProperty] string forumAdmined;
-
         public Admin(string name, string password) : base(name, password) {}
-        public Admin(string name, string password, string forum) : base(name, password) { forumAdmined = forum; }
     }
 }
